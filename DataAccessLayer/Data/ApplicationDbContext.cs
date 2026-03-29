f1513e0d3d449b9e4f4a5074e2c7ab2b3d8c10ac
tubietools <admin@tubietools.com>
2026-03-28 22:03:26 -0500

    unable to login to my sql server Update-Database -Context KitContext 99% operational

--- DataAccessLayer/Data/ApplicationDbContext.cs
+++ DataAccessLayer/Data/ApplicationDbContext.cs
@@ -11,6 +11,8 @@ namespace DataAccessLayer
     /// <summary>
     /// TypeLoadException: Could not load type 'Microsoft.EntityFrameworkCore.Metadata.Internal.AdHocMapper' from assembly 'Microsoft.EntityFrameworkCore, Version=9.0.2.0, Culture=neutral, PublicKeyToken=adb9793829ddae60'.
     /// Microsoft.EntityFrameworkCore.Infrastructure.EntityFrameworkRelationalServicesBuilder.TryAddCoreServices()
+    /// 
+    /// Unable to create a 'DbContext' of type 'ApplicationDbContext'. The exception 'Unable to resolve service for type 'Microsoft.EntityFrameworkCore.DbContextOptions`1[DataAccessLayer.ApplicationDbContext]' while attempting to activate 'DataAccessLayer.ApplicationDbContext'.' was thrown while attempting to create an instance. For the different patterns supported at design time, see https://go.microsoft.com/fwlink/?linkid=851728
     /// </summary>
     public class ApplicationDbContext : IdentityDbContext
     {
--- DataAccessLayer/Data/KitContext.cs
+++ DataAccessLayer/Data/KitContext.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccessLayer
 {
+    /// <summary>
+    /// Unable to create a 'DbContext' of type 'KitContext'.
+    /// The exception 'Unable to resolve service for type 
+    /// 'Microsoft.EntityFrameworkCore.DbContextOptions`1[DataAccessLayer.ApplicationDbContext]' 
+    /// while attempting to activate 'DataAccessLayer.ApplicationDbContext'.' was thrown while attempting to create an instance.
+    /// For the different patterns supported at design time, see https://go.microsoft.com/fwlink/?linkid=851728
+    /// </summary>
     public class KitContext : DbContext
     {
 
-        public KitContext(DbContextOptions<KitContext> options) : base(options)
-        {
-        }
+        //public KitContext(DbContextOptions<KitContext> options) : base(options)
+        //{
+        //}
 
         public DbSet<Profile> Profile { get; set; }
         public DbSet<Event> Event { get; set; }
@@ -20,6 +28,19 @@ namespace DataAccessLayer
             modelBuilder.Entity<Event>().ToTable("Event");
             modelBuilder.Entity<EventType>().ToTable("EventType");
             modelBuilder.Entity<ProfileAspNetUsers>().ToTable("ProfileAspnetUsers");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // Ensure correct path
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+                string connectionString = config.GetConnectionString("KitContext");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
         }
     }
 }
