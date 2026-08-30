6dd5b74dd78ce33327eee2da78ad03934839c488
tubietools <admin@tubietools.com>
2026-08-29 20:30:45 -0500

    add some basic algorithm sorting tests

---
+++ TubieTools_Aspire.Tests/Algorithms/SortingService.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TubieTools_Aspire.Tests.Algorithms
+{
+    public interface ISortingService
+    {
+        void IntArrayInsertionSort(int[] data);
+        void IntArrayBubbleSort(int[] data);
+        void ModifiedBubbleSort(int[] data);
+        void IntArrayQuickSort(int[] data);
+        void IntArraySelectionSort(int[] data);
+        //void IntArrayShellSort(int[] data, int[] intervals);
+        void IntArrayShellSortBetter(int[] data);
+        void IntArrayShellSortNaive(int[] data);
+        void ReverseArray(int[] data);
+    }
+
+    public class SortingService : ISortingService
+    {
+        private static void Swap(int[] data, int m, int n)
+        {
+            int temporary = data[m];
+            data[m] = data[n];
+            data[n] = temporary;
+        }
+
+        public void IntArrayInsertionSort(int[] data)
+        {
+            int i, j;
+            int N = data.Length;
+
+            for (j = 1; j < N; j++)
+            {
+                for (i = j; i > 0 && data[i] < data[i - 1]; i--)
+                {
+                    Swap(data, i, i - 1);
+                }
+            }
+        }
+
+        private void IntArrayQuickSort(int[] data, int l, int r)
+        {
+            int i, j;
+            int x;
+
+            i = l;
+            j = r;
+
+            x = data[(l + r) / 2]; /* find pivot item */
+            while (true)
+            {
+                while (data[i] < x)
+                    i++;
+                while (x < data[j])
+                    j--;
+                if (i <= j)
+                {
+                    Swap(data, i, j);
+                    i++;
+                    j--;
+                }
+                if (i > j)
+                    break;
+            }
+            if (l < j)
+                IntArrayQuickSort(data, l, j);
+            if (i < r)
+                IntArrayQuickSort(data, i, r);
+        }
+
+        public void IntArrayQuickSort(int[] data)
+        {
+            IntArrayQuickSort(data, 0, data.Length - 1);
+        }
+
+        static int[] GenerateIntervals(int n)
+        {
+            if (n < 2)
+            {  // no sorting will be needed
+                return new int[0];
+            }
+            int t = Math.Max(1, (int)Math.Log(n, 3) - 1);
+            int[] intervals = new int[t];
+            intervals[0] = 1;
+            for (int i = 1; i < t; i++)
+                intervals[i] = 3 * intervals[i - 1] + 1;
+            return intervals;
+        }
+
+        public void IntArrayShellSortBetter(int[] data)
+        {
+            int[] intervals = GenerateIntervals(data.Length);
+            IntArrayShellSort(data, intervals);
+        }
+
+        public void IntArrayShellSortNaive(int[] data)
+        {
+            int[] intervals = { 1, 2, 4, 8 };
+            IntArrayShellSort(data, intervals);
+        }
+
+        public void IntArrayBubbleSort(int[] data)
+        {
+            int i, j;
+            int N = data.Length;
+
+            for (j = N - 1; j > 0; j--)
+            {
+                for (i = 0; i < j; i++)
+                {
+                    if (data[i] > data[i + 1])
+                        Swap(data, i, i + 1);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        public void ModifiedBubbleSort(int[] data)
+        {
+            int numberOfSwaps = 0;
+            int n = data.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                // Track number of elements swapped during a single array traversal
+
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    // Swap adjacent elements if they are in decreasing order
+                    if (data[j] > data[j + 1])
+                    {
+                        int num1 = data[j];
+                        int num2 = data[j + 1];
+                        data[j] = num2;
+                        data[j + 1] = num1;
+                        numberOfSwaps++;
+                    }
+                }
+
+                // If no elements were swapped during a traversal, array is sorted
+                if (numberOfSwaps == 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        public int IntArrayMin(int[] data, int start)
+        {
+            int minPos = start;
+            for (int pos = start + 1; pos < data.Length; pos++)
+                if (data[pos] < data[minPos])
+                    minPos = pos;
+            return minPos;
+        }
+
+        public void IntArraySelectionSort(int[] data)
+        {
+            int i;
+            int N = data.Length;
+
+            for (i = 0; i < N - 1; i++)
+            {
+                int k = IntArrayMin(data, i);
+                if (i != k)
+                    Swap(data, i, k);
+            }
+        }
+
+        private void IntArrayShellSort(int[] data, int[] intervals)
+        {
+            int i, j, k, m;
+            int N = data.Length;
+
+            // The intervals for the shell sort must be sorted, ascending
+
+            for (k = intervals.Length - 1; k >= 0; k--)
+            {
+                int interval = intervals[k];
+                for (m = 0; m < interval; m++)
+                {
+                    for (j = m + interval; j < N; j += interval)
+                    {
+                        for (i = j; i >= interval && data[i] < data[i - interval]; i -= interval)
+                        {
+                            Swap(data, i, i - interval);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ReverseArray(int[] data)
+        {
+            int i = 0;
+            int j = data.Length - 1;
+            while (i < j)
+            {
+                var temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+                i++;
+                j--;
+            }
+        }
+    }
+}
---
+++ TubieTools_Aspire.Tests/Algorithms/UnitTestSorting.cs
@@ -0,0 +1,269 @@
+﻿namespace TubieTools_Aspire.Tests.Algorithms
+{
+    [TestClass]
+    public class UnitTestSorting
+    {
+        ISortingService service = new SortingService();
+        // ramp this value up to 500,000 and watch certain algorithms fail to complete in a reasonable time frame
+        readonly int[] data = new int[1000];
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            Random r = new Random();
+            IntArrayGenerate(data, r.Next(1, Int32.MaxValue));
+        }
+
+        private static void IntArrayGenerate(int[] data, int randomSeed)
+        {
+            Random r = new Random(randomSeed);
+            for (int i = 0; i < data.Length; i++)
+                data[i] = r.Next();
+        }
+
+
+        [TestMethod]
+        public void TestSortChars()
+        {
+            char chA = 'A';
+            char ch1 = '1';
+            string str = "test string";
+
+            char chF = 'F';
+            TestContext.WriteLine(Char.GetNumericValue(chF).ToString());
+
+            TestContext.WriteLine(chA.CompareTo('B').ToString());          //-----------  Output: "-1" (meaning 'A' is 1 less than 'B')
+            TestContext.WriteLine(chA.Equals('A').ToString());             //-----------  Output: "True"
+            TestContext.WriteLine(Char.GetNumericValue(ch1).ToString());   //-----------  Output: "1"
+            TestContext.WriteLine(Char.IsControl('\t').ToString());        //-----------  Output: "True"
+            TestContext.WriteLine(Char.IsDigit(ch1).ToString());           //-----------  Output: "True"
+            TestContext.WriteLine(Char.IsLetter(',').ToString());          //-----------  Output: "False"
+            TestContext.WriteLine(Char.IsLower('u').ToString());           //-----------  Output: "True"
+            TestContext.WriteLine(Char.IsNumber(ch1).ToString());          //-----------  Output: "True"
+            TestContext.WriteLine(Char.IsPunctuation('.').ToString());     //-----------  Output: "True"
+            TestContext.WriteLine(Char.IsSeparator(str, 4).ToString());    //-----------  Output: "True"
+            TestContext.WriteLine(Char.IsSymbol('+').ToString());          //-----------  Output: "True"
+            TestContext.WriteLine(Char.IsWhiteSpace(str, 4).ToString());   //-----------  Output: "True"
+            TestContext.WriteLine(Char.Parse("S").ToString());             //-----------  Output: "S"
+            TestContext.WriteLine(Char.ToLower('M').ToString());           //-----------  Output: "m"
+            TestContext.WriteLine('x'.ToString());
+        }
+        /// <summary>
+        /// if index before and after are same, set to 0
+        /// otherwise set to 1
+        /// for ends, assume for index 0 that left is 0, and for index 8 right is 0
+        /// assume they all change at the same time
+        /// </summary>
+        [TestMethod]
+        public void TestCityArray()
+        {
+            int days = 1;
+            int[] items = { 0, 1, 0, 0, 1, 0, 0 };
+            int[] tempItems = new int[items.Length];
+
+            for (int i = 0; i < days; i++)
+            {
+                for (int j = 0; j <= items.Length - 1; j++)
+                {
+                    if (j > 0 && j < items.Length - 1)
+                    {
+                        TestContext.WriteLine("found values                     j+1 =" + items[j + 1] + "and j-1=" + items[j - 1]);
+                        if (items[j + 1] == items[j - 1])
+                        {
+                            tempItems[j] = 0;
+                        }
+                        else
+                        {
+                            tempItems[j] = 1;
+                        }
+                    }
+                    else if (j == 0)
+                    {
+                        TestContext.WriteLine("found values at beginning of array items of j+1 =  " + items[j]);
+                        // beginning of array assume left value is 0
+                        if (items[j + 1] == 0)
+                        {
+                            tempItems[j] = 0;
+                        }
+                        else
+                        {
+                            tempItems[j] = 1;
+                        }
+                    }
+                    else if (j == items.Length - 1)
+                    {
+                        TestContext.WriteLine("found values end of array items of j= " + items[j]);
+                        // end of array asssume right value is 0
+                        if (items[j] == 0)
+                        {
+                            tempItems[j] = 0;
+                        }
+                        else
+                        {
+                            tempItems[j] = 1;
+                        }
+                    }
+                    else
+                    {
+                        TestContext.WriteLine("failed");
+                    }
+                }
+
+                tempItems.CopyTo(items, 0);
+                tempItems = new int[items.Length];
+
+            }
+
+            Assert.IsNotNull(items);
+
+        }
+
+        [TestMethod]
+        public void QuickSort()
+        {
+            service.IntArrayQuickSort(data);
+            Assert.IsNotNull(data);
+        }
+
+
+
+        [TestMethod]
+        public void TestBubbleSort()
+        {
+            service.IntArrayBubbleSort(data);
+
+
+            Assert.IsNotNull(data);
+        }
+
+        [TestMethod]
+        public void TestModifiedBubble()
+        {
+            // testing more about bubble sort
+            service.ModifiedBubbleSort(data);
+            //  | |
+            // ( | )
+            Assert.IsNotNull(data);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void TestSelectionSort()
+        {
+            service.IntArraySelectionSort(data);
+
+            Assert.IsNotNull(data);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void TestInsertionSort()
+        {
+            service.IntArrayInsertionSort(data);
+
+            Assert.IsNotNull(data);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void TestShellSortNaive()
+        {
+            service.IntArrayShellSortNaive(data);
+
+            Assert.IsNotNull(data);
+        }
+
+        [TestMethod]
+        public void TestBuildHeap()
+        {
+            int[] arr = { 1, 3, 5, 4, 6, 13, 10,
+                    9, 8, 15, 17 };
+
+            int n = arr.Length;
+
+            BuildHeap.buildHeap(arr, n);
+
+            BuildHeap.printHeap(arr, n);
+
+            Assert.IsNotNull(arr);
+        }
+
+        //[TestMethod]
+        //public void QuadtraticEquation()
+        //{
+        //    var quadFormula = "b^2+-SquareRoot(b^2-4ac) / 2a";
+        //    var problem = "2x^2 + 8x - 6 = 0";
+        //    // divide by 2
+        //    problem = "x^2 + 4x = 3";
+        //    // chop in 4x two
+        //    problem = @"[2x][2^2]
+        //                [x^2][2x] = 3 +[2^2]"; // ==7
+        //    problem = "(x+2)^2 = 7";
+        //    problem = "x = -2 + SquareRoot(7)";
+        //    // keep equality
+
+        //}
+
+        /// <summary>
+        /// https://www.youtube.com/watch?v=N-KXStupwsc
+        /// nicolo' tartaglia 1500 era
+        /// scipione del ferro
+        /// gerolamo cardano
+        /// </summary>
+        //[TestMethod]
+        //public void SecretCubicFormula()
+        //{
+        //    var formula = "ax^3+bx^2+cx+d=0";
+        //    // set a = 1;
+        //    // set b = 0;
+        //    // change c/d = p/q
+        //    var newFormula = "x^3 +px +q =0";
+
+
+        //}
+
+        [TestMethod]
+        public void TestLog()
+        {
+            var value = Math.Log(81, 3);
+            var f = Math.Log(data.Length, 3) - 1;
+            //var g = Math.Max(1, f);
+            Assert.IsNotNull(value);
+            Assert.IsNotNull(f);
+        }
+        [TestMethod]
+        public void TestShellSortBetterSort()
+        {
+            service.IntArrayShellSortBetter(data);
+
+            Assert.IsNotNull(data);
+        }
+
+        [TestMethod]
+        public void ReverseArray()
+        {
+            int[] array = { 1, 4, 3, 2 };
+            service.ReverseArray(array);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
